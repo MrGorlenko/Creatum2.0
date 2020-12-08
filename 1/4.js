cbd8c8c51f2abdfb2455b2ca290f593ae0b12c56
unknown <tema7198@gmail.com>
2020-12-08 14:08:30 +0300

    clean up

--- 1/4.js
+++
@@ -1,9 +0,0 @@
-    $('.services__tabs button').each(function(i){
-        $(this).on('click', function(k){
-            $(this).addClass('services__tab-btn_active')
-            $(this).siblings().removeClass('services__tab-btn_active')
-            
-            $(`.service-main-navigation-second-type-item:nth-child(${k})`)
-            
-        }) 
-    })
